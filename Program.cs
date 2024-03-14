f05c33cae8146c711be09811af133bb136445ca9
charlton-christ <138693953+charlton-christ@users.noreply.github.com>
2024-03-14 07:14:11 +0530

    Added Task 1 in Coding

--- Program.cs
+++ Program.cs
@@ -1,32 +1,23 @@
-﻿class Program
+﻿namespace Task1_Assignment;
+
+class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter the weight: ");
-        float parcelWeights = float.Parse(Console.ReadLine());// Example weights for parcels
+        string status = "Processing";
 
-        
-        string category = CategorizeParcel(parcelWeights);
-        Console.WriteLine("A parcel weighing "+parcelWeights+"g is categorized as "+category);
-    }
-
-    static string CategorizeParcel(float weight)
-    {
-        string category = "";
-
-        switch (weight)
+        if(status == "Cancelled")
         {
-            case float n when (n < 500.0):
-                category = "Light";
-                break;
-            case float n when (n >= 500.0 && n < 900.0):
-                category = "Medium";
-                break;
-            case float n when (n>=900.0):
-                category = "Heavy";
-                break;
+            Console.WriteLine("The order is Cancelled");
         }
-
-        return category;
+        else if(status == "Processing")
+        {
+            Console.WriteLine("The order is under processing"); ;
+        }
+        else if(status == "Delivered")
+        {
+            Console.WriteLine("The order is Delivered");
+        }
     }
 }
+
