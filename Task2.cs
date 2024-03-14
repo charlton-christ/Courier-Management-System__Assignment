56b3d1f63a846b9e7fe9eb1f4d1893f78adc6de7
charlton-christ <138693953+charlton-christ@users.noreply.github.com>
2024-03-14 07:14:37 +0530

    Added Task 2 in Coding

---
+++ Task2.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the weight: ");
+        float parcelWeights = float.Parse(Console.ReadLine());// Example weights for parcels
+
+        
+        string category = CategorizeParcel(parcelWeights);
+        Console.WriteLine("A parcel weighing "+parcelWeights+"g is categorized as "+category);
+    }
+
+    static string CategorizeParcel(float weight)
+    {
+        string category = "";
+
+        switch (weight)
+        {
+            case float n when (n < 500.0):
+                category = "Light";
+                break;
+            case float n when (n >= 500.0 && n < 900.0):
+                category = "Medium";
+                break;
+            case float n when (n>=900.0):
+                category = "Heavy";
+                break;
+        }
+
+        return category;
+    }
+}
