118454fe8287b8c08b09e122d76cbd1ad86e3f76
charlton-christ <138693953+charlton-christ@users.noreply.github.com>
2024-03-14 06:36:38 +0530

    Added Task 1 in Coding

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task1_Assignment;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string status = "Processing";
+
+        if(status == "Cancelled")
+        {
+            Console.WriteLine("The order is Cancelled");
+        }
+        else if(status == "Processing")
+        {
+            Console.WriteLine("The order is under processing"); ;
+        }
+        else if(status == "Delivered")
+        {
+            Console.WriteLine("The order is Delivered");
+        }
+    }
+}
+
